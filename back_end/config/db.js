5909f4f206de4d4c9c74a1f9b179ea4161701af0
leandro-si <leandro.almeida@uvvnet.com.br>
2025-04-15 11:25:56 -0300

    conexão com o banco

--- back_end/config/db.js
+++ back_end/config/db.js
@@ -1,3 +1,18 @@
 const mongoose = require("mongoose")
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
 
-// connection
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(`mongodb://localhost:27017`)
+
+        console.log("Conectou ao banco!");
+        return dbConn   
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+conn()
+
+module.exports = conn;
