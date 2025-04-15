bd5a684703024c12ec0ee0ca85d58f004efd3746
leandro-si <leandro.almeida@uvvnet.com.br>
2025-04-15 15:42:21 -0300

    criando controller de usuário

--- back_end/routes/Router.js
+++ back_end/routes/Router.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const router = express()
 
+router.use("/api/users", require("./UserRoutes"))
+
 // test route
 router.get("/test", (req, res) => {
     res.send("API Working!!")
