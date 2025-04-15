6e6d197074c063524e3c1763c161eaa4627b91aa
leandro-si <leandro.almeida@uvvnet.com.br>
2025-04-15 16:25:43 -0300

    registro de usuário

--- back_end/config/db.js
+++ back_end/config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose")
 const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASSWORD;
+const dataBase = process.env.DATA_BASE;
 
 const conn = async () => {
     try {
-        const dbConn = await mongoose.connect(`mongodb://localhost:27017`)
+        const dbConn = await mongoose.connect(`mongodb://localhost:27017/${dataBase}`)
 
         console.log("Conectou ao banco!");
         return dbConn   
