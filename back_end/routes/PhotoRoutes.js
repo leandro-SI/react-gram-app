ed0b3b35892b2d410c39be1d9dc1975855d38401
leandro-si <leandro.almeida@uvvnet.com.br>
2025-04-23 07:24:21 -0300

    inserindo foto

--- back_end/routes/PhotoRoutes.js
+++ back_end/routes/PhotoRoutes.js
@@ -2,13 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 // Controller
+const { insertPhoto } = require("../controllers/PhotoController")
 
 // Middlewares
 const validate = require("../middlewares/handleValidation");
 const { photoInsertValidation  } = require("../middlewares/photoValidation");
 const authGuard = require('../middlewares/authGuard');
+const imageUpload = require('../middlewares/imageUpload');
 
 // Routes
+router.post("/", authGuard, imageUpload.single("image"), photoInsertValidation(), validate, insertPhoto);
 
 
 module.exports = router;
