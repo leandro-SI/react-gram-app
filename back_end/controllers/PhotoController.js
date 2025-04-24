97ec57047d7dfd4436c27b80b28f4a8b34e3b084
leandro-si <leandro.almeida@uvvnet.com.br>
2025-04-24 07:36:46 -0300

    atualização de foto

--- back_end/controllers/PhotoController.js
+++ back_end/controllers/PhotoController.js
@@ -80,7 +80,7 @@ const getPhotoById = async (req, res) => {
 
     const photo = await Photo.findById(new mongoose.Types.ObjectId(id))
 
-    // Check if hpoto exists
+    // Check if photo exists
     if (!photo) {
         res.status(404).json({errors: ["Foto não encontrada."]})
         return;
@@ -89,10 +89,42 @@ const getPhotoById = async (req, res) => {
     res.status(200).json(photo)
 }
 
+const updatePhoto = async (req, res) => {
+
+    const {id} = req.params
+    const {title} = req.body
+
+    const reqUser = req.user;
+
+    const photo = await Photo.findById(new mongoose.Types.ObjectId(id))
+
+    // Check if photo exists
+    if (!photo) {
+        res.status(404).json({errors: ["Foto não encontrada."]})
+        return;
+    }
+
+    // Check if photo belongs to user
+    if (!photo.userId.equals(reqUser._id)) {
+        res.status(422).json({errors: ["Ocorreu um erro, por favor tente novamente mais tarde."]})
+        return;
+    }
+
+    if (title) {
+        photo.title = title
+    }
+
+    await photo.save()
+
+    res.status(200).json({photo, message: "Foto atualizada com sucesso"})
+
+}
+
 module.exports = {
     insertPhoto,
     deletePhoto,
     getAllPhotos,
     getUserPhotos,
-    getPhotoById
+    getPhotoById,
+    updatePhoto
 }
