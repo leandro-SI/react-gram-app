7a198568a4d5d431759fa45eefa379d583379214
leandro-si <leandro.almeida@uvvnet.com.br>
2025-08-13 07:00:58 -0300

    criando form de login

--- front_end/src/pages/Auth/Login.jsx
+++ front_end/src/pages/Auth/Login.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
 import "./Auth.css";
 
+// Components
+import { Link } from "react-router-dom";
+import Message from "../../components/Message";
+
+// Hooks
+import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from "react-redux";
+
+// Redux
+//import { register, reset } from "../../slices/authSlice";
+
 const Login = () => {
+
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>Login</div>
+    <div id="login">
+      <h2>ReactGram</h2>
+      <p className='subtitle'>Faça o login para ver o que há de novo.</p>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='E-mail' onChange={(e) => setEmail(e.target.value)} value={email || ""} />
+        <input type="password" placeholder='Senha' onChange={(e) => setPassword(e.target.value)} value={password || ""} />
+        <input type="submit" value="Entrar" />
+      </form>
+      <p>Não tem uma conta? <Link to="/register" >Clique aqui</Link> </p>
+    </div>
   )
 }
 
