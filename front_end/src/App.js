d7084fd566ea8ff9d133f2b18a376195d96f183d
leandro-si <leandro.almeida@uvvnet.com.br>
2025-08-13 07:45:45 -0300

    estruturando para de edição de perfil

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -14,6 +14,7 @@ import Register from './pages/Auth/Register';
 // Components
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import EditProfile from './pages/EditProfile/EditProfile';
 
 function App() {
 
@@ -30,6 +31,7 @@ function App() {
         <div class="container">
           <Routes>
             <Route path='/' element={auth ? <Home /> : <Navigate to="/login" />} />
+            <Route path='/profile' element={auth ? <EditProfile /> : <Navigate to="/login" />} />
             <Route path='/login' element={!auth ? <Login /> : <Navigate to="/" /> } />
             <Route path='/register' element={!auth ? <Register /> : <Navigate to="/" />} />
           </Routes>
