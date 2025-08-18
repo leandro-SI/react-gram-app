ba20c156b8369d4e7c495e6933231bf48bc9f7e9
leandro-si <leandro.almeida@uvvnet.com.br>
2025-08-17 23:11:53 -0300

    formulario de postar foto

--- front_end/src/pages/Profile/Profile.jsx
+++ front_end/src/pages/Profile/Profile.jsx
@@ -25,7 +25,9 @@ const Profile = () => {
     const {user, loading} = useSelector((state) => state.user)
     const {user: userAuth} = useSelector((state) => state.auth)
 
-    // photo
+    // new form and edit form refs
+    const newPhotoForm = useRef()
+    const editPhotoForm = useRef()
 
 
     // loading user data
@@ -37,6 +39,10 @@ const Profile = () => {
 
     if (loading) {
         return <p>Carregando...</p>
+    }
+
+    const submitHandle = (e) => {
+        e.preventDefault()
     }
     
 
@@ -51,6 +57,24 @@ const Profile = () => {
                     <p>{user.bio}</p>
                 </div>
             </div>
+            {id === userAuth._id && (
+                <>
+                    <div className="new-photo" ref={newPhotoForm}>
+                        <h3>Compartilhe algum momento eu:</h3>
+                        <form onSubmit={submitHandle}>
+                            <label>
+                                <span>Título para a foto:</span>
+                                <input type="text" placeholder='Insira um titulo' />
+                            </label>
+                            <label>
+                                <span>Imagem:</span>
+                                <input type="file" />
+                            </label>
+                            <input type="submit" value="Postar" />
+                        </form>
+                    </div>
+                </>
+            )}
         </div>
     )
 }
