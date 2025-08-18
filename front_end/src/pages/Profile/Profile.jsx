8e09ff5b4e5adc961bbae391f57f361b1e7f0911
leandro-si <leandro.almeida@uvvnet.com.br>
2025-08-17 22:43:44 -0300

    exibindo dados do perfil

--- front_end/src/pages/Profile/Profile.jsx
+++ front_end/src/pages/Profile/Profile.jsx
@@ -14,11 +14,45 @@ import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 
 // redux
+import { getUserDetails } from './../../slices/userSlice';
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
+
+    const { id } = useParams()
+
+    const dispatch = useDispatch()
+
+    const {user, loading} = useSelector((state) => state.user)
+    const {user: userAuth} = useSelector((state) => state.auth)
+
+    // photo
+
+
+    // loading user data
+    useEffect(() => {
+
+        dispatch(getUserDetails(id))
+
+    }, [dispatch, id])
+
+    if (loading) {
+        return <p>Carregando...</p>
+    }
+    
+
+    return (
+        <div id="profile">
+            <div className="profile-header">
+                {user.profileImage && (
+                    <img src={`${upload}/users/${user.profileImage}`} alt={user.name} />
+                )}
+                <div class="profile-description">
+                    <h2>{user.name}</h2>
+                    <p>{user.bio}</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Profile
