760d1c052238abc30990582a2a1340690517a68a
leandro-si <leandro.almeida@uvvnet.com.br>
2025-08-22 07:18:59 -0300

    service de busca

--- front_end/src/services/photoService.js
+++ front_end/src/services/photoService.js
@@ -146,6 +146,24 @@ const getPhotos = async (token) => {
     }
 }
 
+// Search photo by title
+const searchPhotos = async (query, token) => {
+
+    const config = requestConfig("GET", query, token)
+
+    try {
+        
+        const res = await fetch(api + "/photos/search?q=" + query, config)
+                    .then((res) => res.json())
+                    .catch((err) => err)
+
+        return res;
+
+    } catch (error) {
+        console.log(error)        
+    }
+}
+
 const photoService = {
     publishPhoto,
     getUserPhotos,
@@ -154,7 +172,8 @@ const photoService = {
     getPhoto,
     like,
     comment,
-    getPhotos
+    getPhotos,
+    searchPhotos
 }
 
 export default photoService;
